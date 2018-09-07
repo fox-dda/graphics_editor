446f2f18390c0deb6591b7fe44a0647b505e1a1e
DESKTOP-KQ9U0O6\Дмитрий <fox-dda@yandex.ru>
2018-09-08 00:09:31 +0700

    Реализован класс Line для отрисовки линии

---
+++ Graphics_editor/Graphics_editor/Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Graphics_editor
+{
+    class Line
+    {
+        public float StartDotX;
+        public float StartDotY;
+        public float EndDotX;
+        public float EndDotY;
+
+        public void Draw(Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, StartDotX, StartDotY, EndDotX, EndDotY);
+        }
+
+        public Line(float sdx, float sdy, float edx, float edy)
+        {
+            StartDotX = sdx;
+            StartDotY = sdy;
+            EndDotX = edx;
+            EndDotY = edy;
+        }
+    }
+}
